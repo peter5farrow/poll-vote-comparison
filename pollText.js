8e7a9624a48cd28c6c6b5e98af3c767d699835d4
Peter Farrow <pnfarrow@gmail.com>
2024-08-12 18:47:05 -0600

    complete

--- pollText.js
+++ pollText.js
@@ -11,7 +11,6 @@ function addPerson(personName) {
       people[person][other] = 0;
     }
   }
-
   return;
 }
 
@@ -49,51 +48,111 @@ function seedRandomVotes(peopleObj) {
   return polls;
 }
 
-const realPolls = seedRandomVotes(people);
-console.log(realPolls);
+// const realPolls = seedRandomVotes(people);
+// console.log(realPolls);
 
-// const realPolls = {
-//   poll1: { A: ["alice", "candice", "dylan"], B: ["bob"] },
-//   poll2: { A: ["alice", "bob", "candice"], B: ["dylan"] },
-//   poll3: { A: ["alice", "candice"], B: ["bob", "dylan"] },
-//   poll4: { A: ["alice", "candice"], B: ["bob", "dylan"] },
-//   poll5: { A: ["alice", "dylan"], B: ["bob", "candice"] },
-//   poll6: { A: ["candice", "dylan"], B: ["alice", "bob"] },
-//   poll7: { A: ["alice", "bob", "candice", "dylan"], B: [] },
-//   poll8: { A: ["bob", "candice"], B: ["alice", "dylan"] },
-//   poll9: { A: ["bob", "candice"], B: ["alice", "dylan"] },
-//   poll10: { A: ["dylan"], B: ["alice", "bob", "candice"] },
-//   poll11: { A: ["alice", "bob", "dylan"], B: ["candice"] },
-//   poll12: { A: ["alice"], B: ["bob", "candice", "dylan"] },
-//   poll13: { A: ["alice", "bob", "candice", "dylan"], B: [] },
-//   poll14: { A: ["bob"], B: ["alice", "candice", "dylan"] },
-//   poll15: { A: ["alice"], B: ["bob", "candice", "dylan"] },
-//   poll16: { A: ["alice", "dylan"], B: ["bob", "candice"] },
-//   poll17: { A: ["dylan"], B: ["alice", "bob", "candice"] },
-//   poll18: { A: ["alice", "bob", "candice", "dylan"], B: [] },
-//   poll19: { A: ["dylan"], B: ["alice", "bob", "candice"] },
-//   poll20: { A: ["alice", "bob", "candice"], B: ["dylan"] },
-// };
+const realPolls = {
+  poll1: {
+    A: ["alice", "dylan", "ethan", "fred", "han"],
+    B: ["bob", "candice", "gretchen"],
+  },
+  poll2: {
+    A: ["dylan", "ethan", "fred", "gretchen"],
+    B: ["alice", "bob", "candice", "han"],
+  },
+  poll3: {
+    A: ["candice", "dylan", "ethan", "fred", "han"],
+    B: ["alice", "bob", "gretchen"],
+  },
+  poll4: {
+    A: ["alice", "dylan", "ethan", "han"],
+    B: ["bob", "candice", "fred", "gretchen"],
+  },
+  poll5: {
+    A: ["alice", "bob", "candice", "ethan", "gretchen", "han"],
+    B: ["dylan", "fred"],
+  },
+  poll6: {
+    A: ["bob", "dylan", "ethan"],
+    B: ["alice", "candice", "fred", "gretchen", "han"],
+  },
+  poll7: {
+    A: ["candice", "dylan", "han"],
+    B: ["alice", "bob", "ethan", "fred", "gretchen"],
+  },
+  poll8: {
+    A: ["bob", "candice", "han"],
+    B: ["alice", "dylan", "ethan", "fred", "gretchen"],
+  },
+  poll9: {
+    A: ["alice", "candice", "dylan", "ethan", "fred", "gretchen"],
+    B: ["bob", "han"],
+  },
+  poll10: {
+    A: ["dylan", "fred"],
+    B: ["alice", "bob", "candice", "ethan", "gretchen", "han"],
+  },
+  poll11: {
+    A: ["bob", "candice", "ethan", "fred", "gretchen"],
+    B: ["alice", "dylan", "han"],
+  },
+  poll12: {
+    A: ["alice", "bob", "ethan", "fred", "han"],
+    B: ["candice", "dylan", "gretchen"],
+  },
+  poll13: {
+    A: ["alice", "fred", "gretchen", "han"],
+    B: ["bob", "candice", "dylan", "ethan"],
+  },
+  poll14: {
+    A: ["alice", "candice", "fred", "gretchen", "han"],
+    B: ["bob", "dylan", "ethan"],
+  },
+  poll15: {
+    A: ["bob", "fred"],
+    B: ["alice", "candice", "dylan", "ethan", "gretchen", "han"],
+  },
+  poll16: {
+    A: ["alice", "bob", "candice", "dylan", "fred", "gretchen", "han"],
+    B: ["ethan"],
+  },
+  poll17: {
+    A: ["dylan", "ethan", "fred", "gretchen", "han"],
+    B: ["alice", "bob", "candice"],
+  },
+  poll18: {
+    A: ["alice", "dylan", "ethan", "han"],
+    B: ["bob", "candice", "fred", "gretchen"],
+  },
+  poll19: {
+    A: ["dylan"],
+    B: ["alice", "bob", "candice", "ethan", "fred", "gretchen", "han"],
+  },
+  poll20: {
+    A: ["alice", "candice", "fred"],
+    B: ["bob", "dylan", "ethan", "gretchen", "han"],
+  },
+};
 
 function getTopFiveShared(personName) {
   if (!people[personName]) {
     return "Please enter a valid name";
   }
 
-  for (const poll in polls) {
+  for (const poll in realPolls) {
     let aVotes = realPolls[`${poll}`]["A"];
     let bVotes = realPolls[`${poll}`]["B"];
 
     for (const person of aVotes) {
       const others = aVotes.filter((name) => name !== person);
-      for (const peep of others) {
-        people[person][peep]++;
+      for (const other of others) {
+        people[person][other]++;
       }
     }
     for (const person of bVotes) {
       const others = bVotes.filter((name) => name !== person);
-      for (const peep of others) {
-        people[person][peep]++;
+      for (const other of others) {
+        people[person][other]++;
       }
     }
   }
@@ -106,15 +165,12 @@ function getTopFiveShared(personName) {
 
   if (Object.keys(sortable).length > 5) {
     const entries = Object.entries(sortable);
-
     const firstFiveEntries = entries.slice(0, 5);
-
     const firstFiveObject = Object.fromEntries(firstFiveEntries);
-
     return firstFiveObject;
   } else {
     return sortable;
   }
 }
 
-console.log(getTopFiveShared("alice"));
+console.log(getTopFiveShared("dylan"));
